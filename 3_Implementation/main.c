8eadc150d53f8b325adceb0530f675329af719ff
RIYA <riya45088@gmail.com>
2022-02-10 19:52:52 +0530

    uploaded code

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include "ATM.h"
+
+
+
+
+int main() {
+    
+    int option;
+    float balance = 15000.00;
+    int choose;
+
+    bool again = true;
+
+ 
+
+    while (again) {
+    mainMenu();
+
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("Your Selection:\t");
+    scanf("%d", &option);
+
+
+        switch (option) {
+            case 1:
+            	system("CLS");
+                checkBankBalance(balance);
+                break;
+            case 2:
+            	system("CLS");
+                balance = depositMoney(balance);
+                break;
+            case 3:
+            	system("CLS");
+                balance = withdrawMoney(balance);
+                break;
+
+            case 4:
+            	system("CLS");
+                exitMenu();
+                return 0;
+
+            default:
+                faultMessage();
+                break;
+        }
+
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("Would you like to do another transaction:\n");
+        printf("< 1 > Yes\n");
+        printf("< 2 > No\n");
+        scanf("%d", &choose);
+
+        system("CLS");
+
+
+
+        if (choose == 2) {
+            again = false;
+            exitMenu();
+
+        }
+
+}
+
+
+    return 0;
+}
 
